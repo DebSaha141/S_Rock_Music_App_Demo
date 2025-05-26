9735f490458c78ce240895e067cc536861b7ab14
DebSaha141 <debsaha141@gmail.com>
2025-05-26 23:38:49 +0530

    Bottomnavigation bar design updated

--- lib/widgets/custom_bottom_nav.dart
+++ lib/widgets/custom_bottom_nav.dart
@@ -17,12 +17,7 @@ class CustomBottomNav extends StatelessWidget {
       height: 75,
       decoration: BoxDecoration(
         color: Color(0XFF18171C),
-        border: Border(
-          top: BorderSide(
-            color: Color(0XFF2C2D31), 
-            width: 1.0, 
-          ),
-        ),
+        border: Border(top: BorderSide(color: Color(0XFF2C2D31), width: 1.0)),
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(10),
           topRight: Radius.circular(10),
@@ -41,20 +36,37 @@ class CustomBottomNav extends StatelessWidget {
   }
 
   Widget _buildNavItem(String iconImage, String label, bool isActive) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset(iconImage, width: 23, height: 23),
-        const SizedBox(height: 4),
-        Text(
-          label,
-          style: GoogleFonts.syne(
-            fontSize: 11,
-            color: isActive ? Colors.white : Color(0XFF61616B),
-            fontWeight: FontWeight.w700,
+    return SizedBox(
+      width: 60,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          if (isActive)
+            Positioned(
+              top: 0,
+              child: Image.asset(
+                "assets/images/ellipse.png",
+                width: 14,
+                height: 7,
+              ),
+            ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(iconImage, width: 23, height: 23),
+              const SizedBox(height: 4),
+              Text(
+                label,
+                style: GoogleFonts.syne(
+                  fontSize: 11,
+                  color: isActive ? Colors.white : const Color(0XFF61616B),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
